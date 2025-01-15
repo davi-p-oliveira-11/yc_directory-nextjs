e1597ff60ce4352104f065d31fa4a352ba1cd3bf
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-15 19:04:31 -0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,65 @@
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = await client.fetch(STARTUPS_QUERY);
+
+  console.log(JSON.stringify(posts, null, 2));
+
+  /*
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrian" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDjx2nLgfUgdMs85NHOUaGxnbgzHUObm7yOA&s",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+  */
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepeneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
